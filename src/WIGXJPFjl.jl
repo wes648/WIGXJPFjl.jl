d680b5a3d6b2512251bb7408502aa43af4174987
Wes <wes.harper2@gmail.com>
2026-01-09 15:30:46 +0100

    fixed ccall to match new jll package

--- src/WIGXJPFjl.jl
+++ src/WIGXJPFjl.jl
@@ -6,20 +6,20 @@ export wig3j, wig6j, wig9j, wig3jj, wig6jj, wig9jj
 
 function __init__()
    max_two_j = 1000
-   ccall((:wig_table_init, wigxjpf),
+   ccall((:wig_table_init, libwigxjpf),
          Cvoid,
          (Cint, Cint),
          max_two_j, 9)
    if VERSION > v"1.12"
    OncePerThread{Vector{Int}}(
-      ccall((:wig_thread_temp_init, wigxjpf),
+      ccall((:wig_thread_temp_init, libwigxjpf),
             Cvoid,
             (Cint,),
             max_two_j)
    )
    else
    Threads.@threads :static for i in 1:Threads.nthreads()
-      ccall((:wig_thread_temp_init, wigxjpf),
+      ccall((:wig_thread_temp_init, libwigxjpf),
             Cvoid,
             (Cint,),
             max_two_j)
@@ -34,7 +34,7 @@ doubled(f::Float64)::Int = Int(2.0*f)
 
 function wig3jj(j12::Integer, j22::Integer, j32::Integer,
                 m12::Integer, m22::Integer, m32::Integer)
-    ccall((:wig3jj, wigxjpf),
+    ccall((:wig3jj, libwigxjpf),
           Cdouble,
           (Cint, Cint, Cint,
            Cint, Cint, Cint),
@@ -48,7 +48,7 @@ wig3j(j1, j2, j3,
 
 function wig6jj(j12::Integer, j22::Integer, j32::Integer,
                 j42::Integer, j52::Integer, j62::Integer)
-    ccall((:wig6jj, wigxjpf),
+    ccall((:wig6jj, libwigxjpf),
           Cdouble,
           (Cint, Cint, Cint,
            Cint, Cint, Cint),
@@ -63,7 +63,7 @@ wig6j(j1, j2, j3,
 function wig9jj(j12::Integer, j22::Integer, j32::Integer,
                j42::Integer, j52::Integer, j62::Integer,
                j72::Integer, j82::Integer, j92::Integer)
-    ccall((:wig9jj, wigxjpf),
+    ccall((:wig9jj, libwigxjpf),
           Cdouble,
           (Cint, Cint, Cint,
            Cint, Cint, Cint,
