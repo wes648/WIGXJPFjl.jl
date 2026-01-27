faf3d8272e222775099da667a5622536ed333df7
Wes <wes.harper2@gmail.com>
2026-01-27 10:45:34 +0100

    potential multithread fix

--- src/WIGXJPFjl.jl
+++ src/WIGXJPFjl.jl
@@ -10,21 +10,21 @@ function __init__()
          Cvoid,
          (Cint, Cint),
          max_two_j, 9)
-   #if VERSION > v"1.12"
-   #OncePerThread{Vector{Int}}(
-   #   ccall((:wig_thread_temp_init, libwigxjpf),
-   #         Cvoid,
-   #         (Cint,),
-   #         max_two_j)
-   #)
-   #else
+   if VERSION > v"1.12"
+   OncePerThread{Vector{Int}}(
+      ccall((:wig_thread_temp_init, libwigxjpf),
+            Cvoid,
+            (Cint,),
+            max_two_j)
+   )
+   else
    Threads.@threads :static for i in 1:Threads.nthreads()
       ccall((:wig_thread_temp_init, libwigxjpf),
             Cvoid,
             (Cint,),
             max_two_j)
       end
-   #end
+   end
 end
 
 doubled(i::Integer)::Int = 2i
